9051876654f1021857363d9e97b132d4bb8e7dc8
Ma-Kas <115688479+Ma-kas@users.noreply.github.com>
2023-06-30 18:12:27 +0900

    Refactor JS script with module pattern
    Next:
    - Implement game against CPU
    - Implement assignment of selected mark to player1, player2/CPU

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
+// Global Constants
 const X = 'x';
 const CIRCLE = 'circle';
 
+
+// Factory functions
 const playerFactory = (name, mark) => {
   return { name, mark };
 };
@@ -8,134 +11,163 @@ const playerFactory = (name, mark) => {
 const player1 = playerFactory('player 1', 'whatever checkbox selected');
 const player2 = playerFactory('player 2', 'whatever checkbox not selected');
 
-const WINNING_COMBINATIONS = [
-  [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
-  [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
-  [0, 4, 8], [2, 4, 6] // diagonal
-]
 
-let gameBoardArray = {};
+// Modules
+const boardController = (() => {
+  const boardCells = document.querySelectorAll('[data-cell]');
+  const gameBoard = document.getElementById('board');
 
-const boardCells = document.querySelectorAll('[data-cell]');
-const gameBoard = document.getElementById('board');
+  function boardSetup() {
+    const markSelectCheckbox = document.getElementById('mark-selector');
+  
+    boardCells.forEach(cell => {
+      // oneshot with once to prevent click on already occupied cells
+      cell.addEventListener('click', gameController.handleCellClick, { once: true }); 
+    });
+  
+    // add selected mark to board classList
+    gameController.currentTurn = markSelectCheckbox.checked ? CIRCLE : X;
+    // on game start, set markTurn to what player selected
+    gameBoard.classList.remove(X);
+    gameBoard.classList.remove(CIRCLE);
+    gameBoard.classList.add(gameController.currentTurn);
+  }
 
-const resultContainer = document.getElementById('result-container');
-const resultMessage = document.querySelector('[data-result-card-text]');
-const resetBtn = document.getElementById('reset-button');
-const exitBtn = document.getElementById('exit-button');
+  return {
+    gameBoard,
+    boardCells,
+    boardSetup,
+  }
+})();
 
-let currentTurn = CIRCLE;
 
-resetBtn.addEventListener('click', (e) => {
-  resetGame();
-});
+const gameController = (() => {
+  const WINNING_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+    [0, 4, 8], [2, 4, 6] // diagonal
+  ]
+  const _resetBtn = document.getElementById('reset-button');
+  const _exitBtn = document.getElementById('exit-button');
+  let currentTurn = CIRCLE;
 
-exitBtn.addEventListener('click', (e) => {
-  resetGame();
-  startGame();
-});
+  function startGame() {
+    const welcomeContainer = document.getElementById('welcome-container');
+    const singlePlayerBtn = document.getElementById('single-player-button');
+    const twoPlayerBtn = document.getElementById('two-player-button');
+  
+    welcomeContainer.classList.remove('hidden');
+  
+    singlePlayerBtn.addEventListener('click', (e) => {
+      displayController.togglePopupVisibility(welcomeContainer, 'hide');
+      boardController.boardSetup();
+      // Start a game against CPU
+    });
+    
+    twoPlayerBtn.addEventListener('click', (e) => {
+      displayController.togglePopupVisibility(welcomeContainer, 'hide');
+      boardController.boardSetup();
+      // Start a game against other player
+    });
+  }
 
-startGame();
+  function handleCellClick(e) {
+    const currentCell = e.target;
+  
+    placeMark(currentCell);
+    if (checkForWin()) {
+      endGame(false);
+    } else if (checkForDraw()) {
+      endGame(true);
+    } else {
+      switchTurn();
+    }
+  }
 
-function startGame() {
-  const welcomeContainer = document.getElementById('welcome-container');
-  const singlePlayerBtn = document.getElementById('single-player-button');
-  const twoPlayerBtn = document.getElementById('two-player-button');
+  function placeMark(currentCell) {
+    currentCell.classList.add(currentTurn);
+  }
 
-  welcomeContainer.classList.remove('hidden');
+  function checkForWin() {
+    return WINNING_COMBINATIONS.some(combination => {
+      return combination.every(index => {
+        return boardController.boardCells[index].classList.contains(currentTurn);
+      });
+    });
+  }
+  
+  function checkForDraw() {
+    // boardCells is NodeList, not array, so need to spread first with [...]
+    return [...boardController.boardCells].every(cell => {
+      return cell.classList.contains(X) || cell.classList.contains(CIRCLE);
+    });
+  }
 
-  singlePlayerBtn.addEventListener('click', (e) => {
-    togglePopupVisibility(welcomeContainer, 'hide');
-    boardSetup();
-    // Start a game against CPU
+  function switchTurn() {
+    boardController.gameBoard.classList.remove(currentTurn);
+    currentTurn = (currentTurn === CIRCLE) ? X : CIRCLE;
+    boardController.gameBoard.classList.add(currentTurn);
+  }
+  
+  function endGame(isDraw) {
+    if (isDraw) {
+      displayController.resultMessage.textContent = `Draw!`;
+    } else {
+      displayController.resultMessage.textContent = `${currentTurn} wins!`;
+    }
+    displayController.togglePopupVisibility(displayController.resultContainer, 'show');
+  }
+
+  function resetGame() {
+    boardController.boardCells.forEach(cell => {
+      cell.classList.remove(X);
+      cell.classList.remove(CIRCLE);
+    });
+  
+    boardController.boardSetup();
+  
+    displayController.togglePopupVisibility(displayController.resultContainer, 'hide');
+  }
+
+  _resetBtn.addEventListener('click', (e) => {
+    resetGame();
   });
   
-  twoPlayerBtn.addEventListener('click', (e) => {
-    togglePopupVisibility(welcomeContainer, 'hide');
-    boardSetup();
-    // Start a game against other player
+  _exitBtn.addEventListener('click', (e) => {
+    resetGame();
+    startGame();
   });
-}
 
-function togglePopupVisibility(target, state) {
-  if (state === 'hide') {
-    target.classList.add('hidden');
-  } else {
-    target.classList.remove('hidden');
+  return {
+    currentTurn,
+    startGame,
+    handleCellClick,
+    checkForWin,
+    checkForDraw,
+    switchTurn,
+    endGame,
+    resetGame,
   }
-}
+})();
 
-function handleCellClick(e) {
-  const currentCell = e.target;
 
-  placeMark(currentCell);
-  if (checkForWin()) {
-    endGame(false);
-  } else if (checkForDraw()) {
-    endGame(true);
-  } else {
-    switchTurn();
+const displayController = (() => {
+  const resultContainer = document.getElementById('result-container');
+  const resultMessage = document.querySelector('[data-result-card-text]');
+ 
+  function togglePopupVisibility(target, state) {
+    if (state === 'hide') {
+      target.classList.add('hidden');
+    } else {
+      target.classList.remove('hidden');
+    }
   }
-}
 
-function placeMark(currentCell) {
-  currentCell.dataset.cell = currentTurn;
-  currentCell.classList.add(currentTurn);
-}
-
-function switchTurn() {
-  board.classList.remove(currentTurn);
-  currentTurn = (currentTurn === CIRCLE) ? X : CIRCLE;
-  board.classList.add(currentTurn);
-}
-
-function boardSetup() {
-  const markSelectCheckbox = document.getElementById('mark-selector');
-
-  boardCells.forEach(cell => {
-    // oneshot with once to prevent click on already occupied cells
-    cell.addEventListener('click', handleCellClick, { once: true }); 
-  });
-
-  // add selected mark to board classList
-  currentTurn = markSelectCheckbox.checked ? CIRCLE : X;
-  // on game start, set markTurn to what player selected
-  board.classList.remove(X);
-  board.classList.remove(CIRCLE);
-  board.classList.add(currentTurn);
-}
-
-function checkForWin() {
-  return WINNING_COMBINATIONS.some(combination => {
-    return combination.every(index => {
-      return boardCells[index].classList.contains(currentTurn);
-    });
-  });
-}
-
-function checkForDraw() {
-  // boardCells is NodeList, not array, so need to spread first with [...]
-  return [...boardCells].every(cell => {
-    return cell.classList.contains(X) || cell.classList.contains(CIRCLE);
-  });
-}
-
-function endGame(isDraw) {
-  if (isDraw) {
-    resultMessage.textContent = `Draw!`;
-  } else {
-    resultMessage.textContent = `${currentTurn} wins!`;
+  return {
+    resultContainer,
+    resultMessage,
+    togglePopupVisibility,
   }
-  togglePopupVisibility(resultContainer, 'show');
-}
+})();
 
-function resetGame() {
-  boardCells.forEach(cell => {
-    cell.classList.remove(X);
-    cell.classList.remove(CIRCLE);
-  });
-
-  boardSetup();
-
-  togglePopupVisibility(resultContainer, 'hide');
-}
+gameController.startGame();
